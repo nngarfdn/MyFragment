89b8fb98e5fc32b9c67df841c50d3e7ba2061c5e
nanang <nanangarif404@gmail.com>
2023-02-21 18:19:13 +0700

    dinamis fragment

--- app/src/main/java/com/android/myfragment/CategoryFragment.java
+++ app/src/main/java/com/android/myfragment/CategoryFragment.java
@@ -12,37 +12,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    public HomeFragment() {
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+
+    public CategoryFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        return inflater.inflate(R.layout.fragment_category, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button btnCategory = view.findViewById(R.id.btn_category);
-        btnCategory.setOnClickListener(this);
+        super.onViewCreated(view, savedInstanceState);
+        Button btnToHome = view.findViewById(R.id.btn_to_home);
+        btnToHome.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_category) {
-            CategoryFragment mCategoryFragment = new CategoryFragment();
+        if (v.getId() == R.id.btn_to_home) {
+            //back like on activity result
             FragmentManager mFragmentManager = getParentFragmentManager();
-            mFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName())
-                    .addToBackStack(null)
-                    .commit();
+            mFragmentManager.popBackStack();
         }
     }
+
 }
